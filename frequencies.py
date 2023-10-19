132ed3fb7993b0fe4d45b6bd712afdf174f732de
Shayna Gail Velasquez <sgavelasquez@gmail.com>
2023-10-19 03:11:11 +0100

    SGAV-frequencies-ans1

--- frequencies.py
+++ frequencies.py
@@ -3,6 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
+
     for i in items:
-        pass                  
+        key = str(i)
+        if key in frequencies:
+            frequencies[key] += 1
+        else:
+            frequencies[key] = 1
+                           
     return frequencies
